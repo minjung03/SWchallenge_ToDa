de6c09c6ec729cfc08187bda532b1f9cea85b38b
kkchans <rlacksgml99@naver.com>
2021-11-04 20:29:27 +0900

    찬희 백그라운드 작업

--- app/src/main/java/com/cookandroid/to_da_project/SettingBackground.java
+++ app/src/main/java/com/cookandroid/to_da_project/SettingBackground.java
@@ -1,6 +1,7 @@
 package com.cookandroid.to_da_project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,29 @@ public class SettingBackground extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_background_color);
+
+        SharedPreferences preferences = getSharedPreferences("change_color", MODE_PRIVATE);
+        Button SettingbackButton = (Button)findViewById(R.id.btn_setting_back);
+        Button Color1Button = (Button)findViewById(R.id.btnColor1);
+
+        SettingbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Setting.class);
+                startActivity(intent);
+
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        });
+
+        Color1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("color1", "#FFDEFA");
+                editor.commit();
+            }
+        });
 
 
     }
--- app/src/main/java/com/cookandroid/to_da_project/TodoList.java
+++ app/src/main/java/com/cookandroid/to_da_project/TodoList.java
@@ -2,12 +2,17 @@
 package com.cookandroid.to_da_project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ResourceBundle;
 
 public class TodoList extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,6 +20,12 @@ public class TodoList extends AppCompatActivity {
         setContentView(R.layout.todolist_list);
 
         Button ListAddButton = (Button) findViewById(R.id.btn_ListAdd);
+
+        SharedPreferences preferences = getSharedPreferences("change_color", MODE_PRIVATE);
+        String n = preferences.getString("color1", "#FFDEFA");
+
+        LinearLayout TodolistLayout = findViewById(R.id.Todolist_list);
+        TodolistLayout.setBackgroundColor(Color.parseColor(n));
 
         ListAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
