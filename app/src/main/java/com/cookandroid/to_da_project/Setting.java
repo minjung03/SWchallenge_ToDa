e04b61291ebb77f6d260b442a31426b348ea3ede
kkchans <rlacksgml99@naver.com>
2021-11-21 22:01:03 +0900

    (찬희) 그림

--- app/src/main/java/com/cookandroid/to_da_project/Setting.java
+++ app/src/main/java/com/cookandroid/to_da_project/Setting.java
@@ -1,9 +1,14 @@
 package com.cookandroid.to_da_project;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,10 @@ public class Setting extends AppCompatActivity {
 
     Button btn_setting_Background, btn_setting_NameChange, btn_setting_IdPassChange,
             btn_setting_back, btn_setting_secession, btn_setting_logout;
+
+    //TextView Rogin_R;
+
+  //  Switch Rogin_switch;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,34 @@ public class Setting extends AppCompatActivity {
         btn_setting_back = (Button)findViewById(R.id.btn_setting_back);
         btn_setting_secession = findViewById(R.id.btn_setting_secession);
         btn_setting_logout = findViewById(R.id.btn_setting_logout);
+       // Rogin_switch = findViewById(R.id.switch_pushAlarm);
+       // Rogin_R = findViewById(R.id.Rogin_R);
+
+       /* Rogin_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+                // 스위치 버튼이 체크되었는지 검사하여 텍스트뷰에 각 경우에 맞게 출력합니다.
+                if (isChecked){
+
+                    SharedPreferences auto = getSharedPreferences("autoLogin", Activity.MODE_PRIVATE);
+                    SharedPreferences.Editor autoLoginEdit = auto.edit();
+                    autoLoginEdit.putString("userId", userId);
+                    autoLoginEdit.putString("passwordNo", passwordNo);
+                    autoLoginEdit.putString("userRole", loginInfo.getUserRole());
+                    autoLoginEdit.putString("userName", loginInfo.getUserNm());
+                    autoLoginEdit.commit();
+                    Rogin_R.setText("옵션 활성화");
+
+                }else{
+
+                    Rogin_R.setText("옵션 비활성화");
+                }
+            }
+        });
+*/
 
         btn_setting_Background.setOnClickListener(new View.OnClickListener() {
             @Override
